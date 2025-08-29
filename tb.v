c16fd839b5a93be46e74f42aa6032ba0091db295
Alokdivedi <alokdivedi77@gmail.com>
2025-08-29 17:15:23 +0530

    Sequence_detector.v

--- tb.v
+++ tb.v
@@ -1,64 +1,46 @@
+// Code your testbench here
+// or browse Examples
 
-module tb_vending_machine;
-
-   
-    reg clk;
-    reg reset;
-    reg nickel_in;
-    reg dime_in;
-    reg quarter_in;
-
-   
-    wire soda_out;
-    wire [3:0] change_out;
-
-  vending_machine uut (
-        .clk(clk),
-        .reset(reset),
-        .nickel_in(nickel_in),
-        .dime_in(dime_in),
-        .quarter_in(quarter_in),
-        .soda_out(soda_out),
-        .change_out(change_out)
-    );
-
-    // Clock generation
-    initial begin
-        clk = 0;
-        forever #5 clk = ~clk; // Toggle clock every 5 time units
-    end
-
-    // Test sequence
-    initial begin
-        // Initialize inputs
-        reset = 1;
-        nickel_in = 0;
-        dime_in = 0;
-        quarter_in = 0;
-        #10 reset = 0; 
-
-        dime_in = 1; #10; 
-        dime_in = 0; #10;
-        nickel_in = 1; #10; 
-        nickel_in = 0; #10;
-   
-
-       
-        quarter_in = 1; #10; 
-        quarter_in = 0; #10;
-       
-
-        dime_in = 1; #10; 
-        dime_in = 0; #10;
-        dime_in = 1; #10; 
-        dime_in = 0; #10;
-   
-
-        $stop;
-    end
-initial begin
-  $dumpfile("dump.vcd");
-  $dumpvars;
-end
+module tb;
+  reg clk ;
+  reg rst ;
+  reg din;
+  
+  wire sequence_found;
+  
+  sequence_det uut(
+    .clk(clk),
+    .rst(rst),
+    .din(din),
+    .sequence_found(sequence_found)
+  );
+  
+  initial begin
+  clk = 0;
+  forever #5 clk = ~clk;
+  end
+  
+  initial begin
+    rst = 1;
+    din = 0;
+    #10 rst = 0;
+    
+    din = 1 ; #10;
+      din = 0 ; #10;
+      din = 1 ; #10;
+      din = 1 ; #10;
+      din = 0 ; #10;
+      din = 1 ; #10;
+      din = 0 ; #10;
+    din = 1 ; #10;
+    din = 0 ; #10;
+    $stop;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    
+  end
   
 endmodule
